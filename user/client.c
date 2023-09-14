f608090f9a53f11564edfc1b6a315ba3eaf04f67
kobero98 <matteo.fede@live.it>
2023-09-14 19:03:00 +0000

    correzzione user

--- user/client.c
+++ user/client.c
@@ -15,9 +15,10 @@ int main(){
         printf("1]PUT\n");
         printf("2]GET\n");
         printf("3]INVALIDATE\n");
+        printf("4]Exit\n");
         int scelta;
         scanf("%d",&scelta);
-        while(scelta<1 || scelta>3){
+        while(scelta<1 || scelta>4){
             printf("Valore non valido\n");
             scanf("%d",&scelta); 
         };
@@ -25,9 +26,12 @@ int main(){
         case 1:
             printf("PUT\n");
             printf("inserire stringa:\n");
-            char * buf1;
-            scanf("%ms",&buf1);
-            z=syscall(PUT,buf1,strlen(buf1));
+            char frase[4098];
+            while(getc(stdin)==EOF);
+            scanf("%[^\n]",&frase);
+            while(getc(stdin)==EOF);
+            printf("frase: %s\n",frase);
+            z=syscall(PUT,frase,strlen(frase));
             printf("Put restituisce come risposta: %d\n",z);
             break;
         case 2:
@@ -36,7 +40,7 @@ int main(){
             scanf("%d",&index);
             char buf2[4096];
             z=syscall(GET,index,buf2,4096);
-            printf("la dimensione dei dati letti e: %d",z);
+            printf("la dimensione dei dati letti e: %d\n",z);
             printf("nel blocco %d ho letto %s\n",index,buf2);
             break;
         case 3:
@@ -45,6 +49,9 @@ int main(){
             scanf("%d",&index);
             z=syscall(INVALIDATE,index);
             printf("la funzione ritorna: %d\n",z);
+            break;
+        case 4:
+            return 0;
             break;
        } 
     }while( true );
--- user/put.c
+++ user/put.c
@@ -7,7 +7,20 @@ int main(int argc,char * argv[]){
         printf("Eseguire ./put <stringa da aggiungere>\n");
         return 0;
     }
-    int z=syscall(PUT,argv[1],strlen(argv[1]));
+    char buffer[4098];
+    int i=1;
+    int len=0;
+    for(;i<argc;i++){
+        if(len+strlen(argv[i])>=4097)break;
+        strcpy(buffer+len,argv[i]);
+        len=len+strlen(argv[i]);
+        char *spazio=" ";
+        printf("buffer\n");
+        strcpy(buffer+len,spazio);
+        len=len+1;
+    }
+    printf("buffer=%s\n",buffer);
+    int z=syscall(PUT,buffer,strlen(buffer));
     if(z!=-1)   printf("index: %d\n",z);
     else    printf("Tutto lo spazio é occupato\n");
     return 0;
